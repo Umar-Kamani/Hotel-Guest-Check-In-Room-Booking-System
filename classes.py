6ab09b200e6d5b9097e679f6143227f5b802d2b9
Umar Kamani <umar.kamani@gmail.com>
2025-12-30 20:38:54 +0400

    Room Class

---
+++ classes.py
@@ -0,0 +1,10 @@
+
+class Room:
+    def __init__(self, room_number, room_type, room_status, room_rate, room_condition, room_access_pin, room_capacity):
+        self.room_number = room_number
+        self.room_type = room_type
+        self.room_status = room_status
+        self.room_rate = room_rate
+        self.room_condition = room_condition
+        self.room_access_pin = room_access_pin
+        self.room_capacity = room_capacity
