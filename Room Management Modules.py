0605aea2e7d21d5f3d0d46e56dd23fc84fa05368
Umar Kamani <umar.kamani@gmail.com>
2026-01-05 02:16:25 +0400

    Added Error handling on inputs

--- Room Management Modules.py
+++ Room Management Modules.py
@@ -2,13 +2,20 @@ import classes
 
 def create_new_room():
     print("Welcome to the Room Creation Wizard")
-    new_room_number=input("Please enter the room number: ")
-    while True: #Room type user input check
+    #Room number input
+    while True:
+        new_room_number=input("Please enter the room number: ") #Asks the user for the room number
+        if new_room_number == "":
+            print("Please enter a valid room number.")
+        else:
+            break
+    #Room type input
+    while True: #this loop enables us to validate the correct choice from the user in terms of room types
         print("""Room Types:
               1. Standard Room
               2. Deluxe Room
               3. Suite Room""")
-        new_room_type=input("Please select your room type: ")
+        new_room_type=input("Please select your room type: ") #we prompt the user to enter a number according to the room type
         if new_room_type == '1':
             new_room_type = "Standard Room"
             print(f"Room Type: {new_room_type}.")
@@ -23,9 +30,19 @@ def create_new_room():
             break
         else :
             print("Invalid room type. Please try again.")
+    #Room capacity input
     while True:
         new_room_capacity=input("Please enter the room capacity: ")
-    while True: #Room condition user input check
+        try:
+            new_room_capacity = int(new_room_capacity)
+            if new_room_capacity == "" or new_room_capacity >= 6: #checks validity of room capacity input, max room capacity 6 pax
+                print("Invalid room capacity. Please try again, room capacity can't exceeds 6.")
+            else:
+                break
+        except ValueError:
+            print("Invalid room capacity. Please try again.")
+    #Room condition input
+    while True: #this loop enables us to validate the correct choice from the user in terms of room condition
         print("""Room Condition:
               1. Clean
               2. Dirty""")
@@ -40,8 +57,23 @@ def create_new_room():
             break
         else :
             print("Invalid room condition. Please try again.")
-    new_room_access_pin=input("Please enter the room access pin: ")
-    new_room_rate=input("Please enter the room rate: ")
+    #Room Access Pin input
+    while True:
+        new_room_access_pin=input("Please enter the room access pin: ")
+        try:
+            if not new_room_access_pin.isdigit or new_room_access_pin != 4 or new_room_access_pin == '': #this function checks for a valid 4 digit access pin
+                print("Invalid room access pin. Room access pin must be 4 digits long.")
+            else:
+                break
+        except ValueError:
+            print("Invalid room access pin. Room access pin must be 4 digits long.")
+    #Room Rate input
+    while True:
+        new_room_rate=input("Please enter the room rate: $")
+        if not new_room_rate.isdigit() or new_room_rate == '':
+            print("Invalid room rate. Please try again.")
+        else:
+            break
 
     print(new_room_number)
     print(new_room_type)
