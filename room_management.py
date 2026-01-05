5408839cb3ee238a126c8cd918ddae543da5ca07
Umar Kamani <umar.kamani@gmail.com>
2026-01-05 19:36:47 +0400

    finished create room

--- room_management.py
+++ room_management.py
@@ -1,9 +1,12 @@
-import classes
-
+import room_class
+from tabulate import tabulate
 
 
 def create_new_room():
+
     print("Welcome to the Room Creation Wizard")
+    print("___________________________________")
+    new_room_status = "Empty"
     #Room number input
     while True:
         new_room_number=input("Please enter the room number: ") #Asks the user for the room number
@@ -11,6 +14,8 @@ def create_new_room():
             print("Please enter a valid room number.")
         else:
             break
+
+    print("___________________________________")
     #Room type input
     while True: #this loop enables us to validate the correct choice from the user in terms of room types
         print("""Room Types:
@@ -32,17 +37,21 @@ def create_new_room():
             break
         else :
             print("Invalid room type. Please try again.")
+
+    print("___________________________________")
     #Room capacity input
     while True:
         new_room_capacity=input("Please enter the room capacity: ")
         try:
             new_room_capacity = int(new_room_capacity)
-            if new_room_capacity == "" or new_room_capacity >= 6: #checks validity of room capacity input, max room capacity 6 pax
+            if new_room_capacity > 6: #checks validity of room capacity input, max room capacity 6 pax
                 print("Invalid room capacity. Please try again, room capacity can't exceeds 6.")
             else:
                 break
         except ValueError:
             print("Invalid room capacity. Please try again.")
+
+    print("___________________________________")
     #Room condition input
     while True: #this loop enables us to validate the correct choice from the user in terms of room condition
         print("""Room Condition:
@@ -59,16 +68,20 @@ def create_new_room():
             break
         else :
             print("Invalid room condition. Please try again.")
+
+    print("___________________________________")
     #Room Access Pin input
     while True:
         new_room_access_pin=input("Please enter the room access pin: ")
         try:
-            if not new_room_access_pin.isdigit or len(new_room_access_pin) != 4 or new_room_access_pin == '': #this function checks for a valid 4 digit access pin
+            if not new_room_access_pin.isdigit() or len(new_room_access_pin) != 4: #this function checks for a valid 4 digit access pin
                 print("Invalid room access pin. Room access pin must be 4 digits long.")
             else:
                 break
         except ValueError:
             print("Invalid room access pin. Room access pin must be 4 digits long.")
+
+    print("___________________________________")
     #Room Rate input
     while True:
         new_room_rate=input("Please enter the room rate: $")
@@ -77,15 +90,19 @@ def create_new_room():
         else:
             break
 
-    print(new_room_number)
-    print(new_room_type)
-    print(new_room_condition)
-    print(new_room_access_pin)
-    print(new_room_rate)
-    print(new_room_capacity)
-
-    new_room = classes.Room(new_room_number, new_room_type, "empty", new_room_rate, new_room_condition, new_room_access_pin, new_room_capacity)
-    print(new_room.__dict__)
-
+    print("___________________________________")
+    new_room = room_class.Room(new_room_number, new_room_type, new_room_status, new_room_rate, new_room_condition, new_room_access_pin, new_room_capacity) #makes new room into an object
+    print("___________________________________________________________")
+    print("Room Successfully Created, please verify room details below:\n")
+    room_data = {
+        "Room Number": new_room_number,
+        "Room Type": new_room_type,
+        "Room Capacity": new_room_capacity,
+        "Room Status": new_room_status,
+        "Room Condition": new_room_condition,
+        "Room Access Pin": new_room_access_pin,
+        "Room Rate": new_room_rate,
+    }
+    print(tabulate([room_data], headers="keys", tablefmt="fancy_grid"))
 
 create_new_room()
