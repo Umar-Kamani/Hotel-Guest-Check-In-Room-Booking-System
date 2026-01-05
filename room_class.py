87f4e5c265b4a23be8410ee3c8a297ebe79de266
Umar Kamani <umar.kamani@gmail.com>
2026-01-06 01:19:43 +0400

    Added load up function

--- room_class.py
+++ room_class.py
@@ -4,10 +4,11 @@ import os
 class Room:
     room_registry=[]
     room_id = 0
+    dir_name = "data"
     file_name = "rooms.csv"
-    file_path = f"data/{file_name}"
+    file_path = f"{dir_name}/{file_name}"
 
-    def __init__(self, room_number, room_type, room_status, room_rate, room_condition, room_access_pin, room_capacity):
+    def __init__(self, room_number, room_type, room_capacity, room_status, room_condition, room_access_pin, room_rate, save=True):
         Room.room_id += 1
         self.id = Room.room_id
         self.room_number = room_number
@@ -18,9 +19,11 @@ class Room:
         self.room_access_pin = room_access_pin
         self.room_capacity = room_capacity
         Room.room_registry.append(self)
-        self.save_to_csv()
+        if save:
+            self.save_to_csv()
 
     def save_to_csv(self):
+        os.makedirs("data", exist_ok=True)
         room_file_exists = os.path.exists(Room.file_path)
         with open(Room.file_path, 'a', newline='') as csvfile:
             writer = csv.writer(csvfile)
@@ -45,11 +48,25 @@ class Room:
                 self.room_access_pin,
                 self.room_rate
             ])
-    def load_room_data(self):
-        if not os.path.exists(Room.file_path):
-            return
-        else:
-            with open(Room.file_path, 'r') as csvfile:
-                reader = csv.reader(csvfile)
-                print(reader)
+def load_room_data():
+    if not os.path.exists(Room.file_path):
+        return
+    else:
+        with open(Room.file_path, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                room = Room(
+                    row["Room Number"],
+                    row["Room Type"],
+                    row["Room Capacity"],
+                    row["Room Status"],
+                    row["Room Condition"],
+                    row["Room Access Pin"],
+                    row["Room Rate"],
+                    save = False
+                )
+            room.id = int(row["ID"])
+            Room.room_id = max(room.id, Room.room_id) #compares room_id from line 6 and the one we loaded from the csv file into memory and re-assigns it to the largest number
+            Room.room_registry.append(room) #Appends all rooms from csv to memory
+    return
 
