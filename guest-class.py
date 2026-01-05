25849ebb725f812748668b50feb7f685253f0651
Deen <sumailasherif773@gmail.com>
2026-01-05 18:37:41 +0400

    Add Guest class with various parameters

--- guest-class.py
+++ guest-class.py
@@ -1,1 +1,10 @@
+class Guest:
+    def __init__(self, guest_id, full_name, phone_number, id_number, date_of_birth):
+        self.guest_id = guest_id
+        self.full_name = full_name
+        self.phone_number = phone_number
+        self.id_number = id_number
+        self.date_of_birth = date_of_birth
+
+   
 
