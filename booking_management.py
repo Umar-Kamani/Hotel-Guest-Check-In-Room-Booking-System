cdfc75ec6bf1e0484bc13541df2e30e4b0acb643
Umar Kamani <umar.kamani@gmail.com>
2026-01-12 23:57:56 +0400

    Fixed bug in available room filter, where datetime was compared to str.
    Finished def create_booking

--- booking_management.py
+++ booking_management.py
@@ -68,11 +68,11 @@ def create_booking():
     available_room_filter=[
         t for t in room_class.Room.room_registry
         if t.room_type == booking_room_type
-        and t.room_status == "Empty"
+        and t.room_status != "Occupied"
         and t.room_capacity >= booking_room_capacity
         and not any(
-            datetime.strptime(b.start_date,"%Y-%m-%d") < start_date_booking
-            and datetime.strptime(b.end_date,"%Y-%m-%d") > end_date_booking
+            datetime.strptime(b.start_date,"%Y-%m-%d") < datetime.strptime(start_date_booking,"%Y-%m-%d")
+            and datetime.strptime(b.end_date,"%Y-%m-%d") > datetime.strptime(end_date_booking, "%Y-%m-%d")
             for b in booking_class.Booking.booking_registry
         )]
 
@@ -84,8 +84,8 @@ def create_booking():
                     "Room Type": t.room_type,
                     "Room Capacity": t.room_capacity,
                     "Room Status": t.room_status,
-                    "Booking Start Date": t.start_date,
-                    "Booking End Date": t.end_date
+                    "Room Condition": t.room_condition,
+                    "Room Rate": f"${t.room_rate}",
                 })
                 print(f"The following rooms with type '{booking_room_type}' and capacity '"
                       f"{booking_room_capacity} are available:")
@@ -93,35 +93,34 @@ def create_booking():
     else:
         print(f"No rooms with type '{booking_room_type}' and capacity '{booking_room_capacity} are available\n")
         room_management.view_available_rooms()
+        create_booking()
 
     while True:
         booking_room_number = input("Please enter the room number you wish to book: ")
         try:
             booking_room_number = int(booking_room_number)
-            break
+            if any(booking_room_number == room.room_number for room in available_room_filter):
+                break
+            else:
+                print("Invalid room number. Please choose an available room.")
         except ValueError:
             print("Invalid room number. Please choose an available room.")
 
-        for available in available_room_filter:
-            if booking_room_number == available.room_number:
-                break
-            elif booking_room_number != available.room_number:
-                print("Invalid room number. Please choose an available room.")
     while True:
         guest_id_passport_number = input("Please enter the guest ID/Passport number: ")
         if not guest_id_passport_number.strip():
             print("Invalid guest ID/Passport number. Please try again.")
-        elif any(guest_id_passport_number == guest.id_passport for guest in guest_class.Guest.guest_registry):
+        elif any(guest_id_passport_number == guest.passport_number for guest in guest_class.Guest.guest_registry):
             print("Guest already exist, please find info below.")
             existing_guest=[]
             for t in guest_class.Guest.guest_registry:
-                if t.id_passport == guest_id_passport_number:
+                if t.passport_number == guest_id_passport_number:
                     existing_guest.append({
                         "Guest ID": t.guest_id,
                         "Guest Name": t.full_name,
                         "Guest Phone Number": t.phone_number,
-                        "Guest Date of Birth": t.birth_date,
-                        "Guest ID/Passport": t.id_passport,
+                        "Guest Date of Birth": t.date_of_birth,
+                        "Guest ID/Passport": t.passport_number,
                     })
             print(tabulate(existing_guest, headers="keys", tablefmt="fancy_grid"))
             break
@@ -132,20 +131,35 @@ def create_booking():
 
     new_guest_list=[]
     for guest in guest_class.Guest.guest_registry:
-        if guest.id_passport == guest_id_passport_number:
-            new_guest_pass_id=guest.id_passport
-        booking=booking_class.Booking(guest.guest_id, booking_room_number, "Booked", start_date_booking, end_date_booking)
+        if guest.passport_number == guest_id_passport_number:
+            booking=booking_class.Booking(guest.guest_id, guest.full_name, booking_room_number, "Booked", start_date_booking, end_date_booking)
 
-    print("Booking Complete")
-    #update memory
+    new_booking_filter=[t for t in booking_class.Booking.booking_registry]
+    new_booking=[]
+    for t in new_booking_filter:
+        if t.start_date == start_date_booking:
+            new_booking.append({
+                "Booking ID": t.id,
+                "Guest ID": t.guest_id,
+                "Guest Name": t.guest_name,
+                "Guest Room Number": t.room_number,
+                "Booking Status": t.status,
+                "Booking Start Date": t.start_date,
+                "Booking End Date": t.end_date
+            })
+    print("Booking Complete, please find booking info below.")
+    print(tabulate(new_booking, headers="keys", tablefmt="fancy_grid"))
 
-
-
-
-
+    #update the status of the booked room
+    for t in room_class.Room.room_registry:
+        if t.room_number == booking_room_number:
+            t.room_status = "Booked"
+            room_class.Room.save_after_modification()
 
 
 
 
 room_class.load_room_data()
+guest_class.load_guest_data()
+booking_class.load_booking_data()
 create_booking()
