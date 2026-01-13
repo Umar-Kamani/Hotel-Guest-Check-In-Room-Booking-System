4c0a2cc3ca80b3fbc54d0d545dd0bcecacb99657
Umar Kamani <umar.kamani@gmail.com>
2026-01-14 01:05:43 +0400

    Added missing loop

--- booking_management.py
+++ booking_management.py
@@ -13,79 +13,81 @@ def create_booking():
     print("___________________________________")
     print("Welcome to the Booking Wizard")
     print("___________________________________")
-    #Room type selection
     while True:
-        print("""Room Types:
-                      1. Standard Room
-                      2. Deluxe Room
-                      3. Suite Room""")
-        booking_room_type = input("Please select the room type you wish to book: ")
-        if booking_room_type == '1':
-            booking_room_type = "Standard Room"
-            break
-        if booking_room_type == '2':
-            booking_room_type = "Deluxe Room"
-            break
-        if booking_room_type == '3':
-            booking_room_type = "Suite Room"
+        #Room type selection
+        while True:
+            print("""Room Types:
+                          1. Standard Room
+                          2. Deluxe Room
+                          3. Suite Room""")
+            booking_room_type = input("Please select the room type you wish to book: ")
+            if booking_room_type == '1':
+                booking_room_type = "Standard Room"
+                break
+            if booking_room_type == '2':
+                booking_room_type = "Deluxe Room"
+                break
+            if booking_room_type == '3':
+                booking_room_type = "Suite Room"
+                break
+            else:
+                print("Invalid room type. Please try again.")
+        #Room capacity selection
+        while True:
+            booking_room_capacity = input("Please enter the room capacity you wish to book: ")
+            try:
+                booking_room_capacity = int(booking_room_capacity)
+                if not 0 < booking_room_capacity <= 6: #checks validity of room capacity input, max room capacity 6 pax
+                    print("Invalid room capacity. Please try again, room capacity can't exceeds 6.")
+                else:
+                    break
+            except ValueError:
+                print("Invalid room capacity. Please try again.")
+        #Room booking timeframe input
+        while True:
+            start_date_booking = input("Please enter the start date of booking (YYYY-MM-DD): ")
+            try:
+                datetime.strptime(start_date_booking, "%Y-%m-%d")
+                break
+            except ValueError:
+                print("Invalid start date format. Please use YYYY-MM-DD.")
+        while True:
+            end_date_booking = input("Please enter the end date of booking (YYYY-MM-DD): ")
+            try:
+                datetime.strptime(end_date_booking, "%Y-%m-%d")
+                break
+            except ValueError:
+                print("Invalid end date format. Please use YYYY-MM-DD.")
+
+                #Check if room is available
+        available_room_filter=[
+            t for t in room_class.Room.room_registry
+            if t.room_type == booking_room_type
+            and t.room_capacity >= booking_room_capacity
+            and not any(
+                datetime.strptime(b.start_date,"%Y-%m-%d") <= datetime.strptime(start_date_booking,"%Y-%m-%d")
+                and datetime.strptime(b.end_date,"%Y-%m-%d") >= datetime.strptime(end_date_booking, "%Y-%m-%d")
+                for b in booking_class.Booking.booking_registry
+            )]
+
+        if available_room_filter:
+            available_rooms=[]
+            for t in available_room_filter:
+                    available_rooms.append({
+                        "Room Number": t.room_number,
+                        "Room Type": t.room_type,
+                        "Room Capacity": t.room_capacity,
+                        "Room Condition": t.room_condition,
+                        "Room Rate": f"${t.room_rate}",
+                    })
+                    print(f"The following rooms with type '{booking_room_type}' and capacity '"
+                          f"{booking_room_capacity} are available:")
+                    print(tabulate(available_rooms, headers="keys", tablefmt="fancy_grid"))
             break
         else:
-            print("Invalid room type. Please try again.")
-    #Room capacity selection
-    while True:
-        booking_room_capacity = input("Please enter the room capacity you wish to book: ")
-        try:
-            booking_room_capacity = int(booking_room_capacity)
-            if not 0 < booking_room_capacity <= 6: #checks validity of room capacity input, max room capacity 6 pax
-                print("Invalid room capacity. Please try again, room capacity can't exceeds 6.")
-            else:
-                break
-        except ValueError:
-            print("Invalid room capacity. Please try again.")
-    #Room booking timeframe input
-    while True:
-        start_date_booking = input("Please enter the start date of booking (YYYY-MM-DD): ")
-        try:
-            datetime.strptime(start_date_booking, "%Y-%m-%d")
-            break
-        except ValueError:
-            print("Invalid start date format. Please use YYYY-MM-DD.")
-    while True:
-        end_date_booking = input("Please enter the end date of booking (YYYY-MM-DD): ")
-        try:
-            datetime.strptime(end_date_booking, "%Y-%m-%d")
-            break
-        except ValueError:
-            print("Invalid end date format. Please use YYYY-MM-DD.")
-
-            #Check if room is available
-    available_room_filter=[
-        t for t in room_class.Room.room_registry
-        if t.room_type == booking_room_type
-        and t.room_capacity >= booking_room_capacity
-        and not any(
-            datetime.strptime(b.start_date,"%Y-%m-%d") < datetime.strptime(start_date_booking,"%Y-%m-%d")
-            and datetime.strptime(b.end_date,"%Y-%m-%d") > datetime.strptime(end_date_booking, "%Y-%m-%d")
-            for b in booking_class.Booking.booking_registry
-        )]
-
-    if available_room_filter:
-        available_rooms=[]
-        for t in available_room_filter:
-                available_rooms.append({
-                    "Room Number": t.room_number,
-                    "Room Type": t.room_type,
-                    "Room Capacity": t.room_capacity,
-                    "Room Condition": t.room_condition,
-                    "Room Rate": f"${t.room_rate}",
-                })
-                print(f"The following rooms with type '{booking_room_type}' and capacity '"
-                      f"{booking_room_capacity} are available:")
-                print(tabulate(available_rooms, headers="keys", tablefmt="fancy_grid"))
-    else:
-        print(f"No rooms with type '{booking_room_type}' and capacity '{booking_room_capacity} are available\n")
-        room_management.view_available_rooms()
-        create_booking()
+            print(f"No rooms with type '{booking_room_type}' and capacity '{booking_room_capacity} are available\n")
+            room_management.view_available_rooms()
+            continue
 
     while True:
         booking_room_number = input("Please enter the room number you wish to book: ")
